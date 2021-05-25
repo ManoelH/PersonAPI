7b947a858359763aab1d74ebc0c1786c07e1af44
Manoel Henrique <henriqe115@gmail.com>
2021-05-25 12:03:43 -0300

    implementing findAll and findById of people

--- src/main/java/com/manoelh/personapi/controller/PersonController.java
+++ src/main/java/com/manoelh/personapi/controller/PersonController.java
@@ -2,10 +2,13 @@ package com.manoelh.personapi.controller;
 
 import com.manoelh.personapi.dto.MessageResponseDTO;
 import com.manoelh.personapi.dto.PersonDTO;
+import com.manoelh.personapi.exception.personNotFoundException;
 import com.manoelh.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -23,5 +26,15 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO) {
         return this.personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAllPeople() {
+        return this.personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findOnePerson(@PathVariable Long id) throws personNotFoundException {
+        return this.personService.findOnePerson(id);
     }
 }
--- src/main/java/com/manoelh/personapi/service/PersonService.java
+++ src/main/java/com/manoelh/personapi/service/PersonService.java
@@ -3,10 +3,14 @@ package com.manoelh.personapi.service;
 import com.manoelh.personapi.dto.MessageResponseDTO;
 import com.manoelh.personapi.dto.PersonDTO;
 import com.manoelh.personapi.entity.Person;
+import com.manoelh.personapi.exception.personNotFoundException;
 import com.manoelh.personapi.mapper.PersonMapper;
 import com.manoelh.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -30,5 +34,17 @@ public class PersonService {
         return MessageResponseDTO
                 .builder().message("The person was saved with ID: " +savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = this.personRepository.findAll();
+        return allPeople.stream().map (personMapper::toDTO).collect(Collectors.toList());
+    }
+
+    public PersonDTO findOnePerson(Long id) throws personNotFoundException {
+       Person person = personRepository.findById(id)
+               .orElseThrow(() -> new personNotFoundException(id) );
+
+       return personMapper.toDTO(person);
     }
 }
